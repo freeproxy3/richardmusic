0c7dde48f5da2af465526c02cd7781dea1b74877
freeproxy3 <freeproxy3@gmail.com>
2017-12-02 22:36:39 -0500

    in test2

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows  Test  branch
+@rem  Gradle startup script for Windows  Test  branch MyTest2
 @rem
 @rem ##########################################################################
 
